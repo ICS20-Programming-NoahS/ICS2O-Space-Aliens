372466e0ce2bc1413aaff3e5b99d24ec7d09d0ce
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-06-09 14:39:19 +0000

    resets score to 0

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,6 +8,11 @@
 
 class GameScene extends Phaser.Scene {
 
+  resetScore() {
+    this.score = 0
+    this.scoreText.setText("Score: " + this.score.toString())
+  }
+
   // create an alien
   createAlien () {
 
@@ -93,7 +98,10 @@ class GameScene extends Phaser.Scene {
       shipCollide.destroy()
       this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive({ useHandCursor: true })
-      this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+      this.gameOverText.on("pointerdown", () => {
+        this.resetScore() // reset score
+        this.scene.start("gameScene")
+      })
     }.bind(this))
   }
   
